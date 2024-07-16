df491d60a4cf4475f9a817959b6f46dc48519f99
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-07-16 17:33:32 +0800

    feat: support execute shell command with envs

--- shell/shell.go
+++ shell/shell.go
@@ -2,31 +2,38 @@ package shell
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 // command = exec.Command("sh", "-c", cmd) // waring!!!
-func ExecCommand(name string, args ...string) (output string, errmsg string, errno int) {
+func ExecCommand(command string, args ...string) (output string, errmsg string, errno int) {
+	return ExecCommandWithEnvs(nil, command, args...)
+}
+
+func ExecCommandWithEnvs(envs []string, command string, args ...string) (output string, errmsg string, errno int) {
 	var stderr bytes.Buffer
-	cmd := exec.Command(name, args...)
+	cmd := exec.Command(command, args...)
+	cmd.Env = append(os.Environ(), envs...)
 	cmd.Stderr = &stderr
-	// err := command.Run() // err: exit status 1
+	// err := cmd.Run() // err: exit status 1
 	outputBytes, err := cmd.Output()
-	output = strings.TrimSpace(string(outputBytes))
-	errmsg = strings.TrimSpace(stderr.String())
+	output = string(outputBytes)
+	errmsg = stderr.String()
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			if status, ok := exitErr.Sys().(interface{ ExitStatus() int }); ok {
 				errno = status.ExitStatus()
 			}
-		}else if execErr, ok:= err.(*exec.Error); ok{
-			errmsg = execErr.Error()+" "+errmsg
+		} else if execErr, ok := err.(*exec.Error); ok {
+			errmsg = execErr.Error() + " " + errmsg
 			errno = 127
 		}
 	}
 	return
 }
+
 
 func ExecCommandPipe(cmd string,stdin []byte, args ...string) (output []byte, errmsg string, errno int) {
 	var stdout, stderr bytes.Buffer
